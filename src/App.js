3c5badf18d2351a3238515b4eaa643c80645eb9c
Md Parvez Habib <parvezmd082@gmail.com>
2023-01-14 01:40:35 +0600

    2nd-Jan-14-1.40-Night

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import Home from './Home';
 import Navbar from './Navbar';
+import Create from './Create';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-       <Navbar />
-      <div className="content">
-       <Home />
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+            <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/create" element={<Create />} />
+            </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
